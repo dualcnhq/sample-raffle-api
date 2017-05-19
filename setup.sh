509b47917a21f906a44cae8b2144ffd77e3fc993
Clau Yagyagan <dualcnhq@gmail.com>
2017-05-19 14:59:22 +0800

    Add script to store env_vars for devt and testing locally

---
+++ setup.sh
@@ -0,0 +1,12 @@
+export SECRET_KEY='86a397433bb7c4c8116b3b4b5d763025'
+export STAGE='dev'
+
+# check if venv is present else make venv
+#if [[ ! -e $venv ]]; then
+#    echo 'source venv/bin/activate'
+#elif [[ ! -d $dir ]]; then
+#    echo 'virtualenv --no-site-packages venv'
+#    echo 'source venv/bin/activate'
+#fi
+
+#echo 'python raffle_api.py'
